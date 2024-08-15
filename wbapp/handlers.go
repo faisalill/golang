a1bb27541f02b4d77b4a71c5ff92cf97dd7dfe02
faisalill <mfaisal.ilyas.d@gmail.com>
2024-08-15 22:14:05 +0530

    wbapp templating proto done

--- wbapp/handlers.go
+++ wbapp/handlers.go
@@ -1,9 +1,37 @@
 package main
 
-import "net/http"
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
 
 func homeHandler(res http.ResponseWriter, req *http.Request) {
 	message := "Yuar Path: " + req.URL.Path
 
 	res.Write([]byte(message))
+}
+
+func htmlHandler(res http.ResponseWriter, req *http.Request) {
+	template, err := template.ParseFiles("templates/post.html")
+	if err != nil {
+		log.Fatal("Error: ", err)
+	}
+
+	data := struct {
+		UserId int32
+		Id     int32
+		Title  string
+		Body   string
+	}{
+		UserId: 1,
+		Id:     3,
+		Title:  "Title of the Blog",
+		Body:   "Body of the whole blog. Body of the whole blog.",
+	}
+
+	err = template.Execute(res, data)
+	if err != nil {
+		http.Error(res, err.Error(), 402)
+	}
 }
--- wbapp/main.go
+++ wbapp/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", htmlHandler)
 
 	err := http.ListenAndServe(":8000", nil)
 	if err != nil {
