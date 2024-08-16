0a52c814454e384f1954f92f0d97f709aafb1cf5
faisalill <mfaisal.ilyas.d@gmail.com>
2024-08-16 21:43:44 +0530

    refactor wbapp

--- wbapp/main.go
+++ wbapp/main.go
@@ -1,15 +1,50 @@
 package main
 
 import (
+	"encoding/json"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func main() {
-	http.HandleFunc("/posts", htmlHandler)
+type Post struct {
+	UserId int32
+	Id     int32
+	Title  string
+	Body   string
+}
 
-	err := http.ListenAndServe(":8000", nil)
+func homeHandler(res http.ResponseWriter, req *http.Request) {
+	postsUrl := "https://jsonplaceholder.typicode.com/posts"
+
+	resp, err := http.Get(postsUrl)
 	if err != nil {
-		log.Fatal("Error: ", err)
+		panic(err)
 	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var Posts []Post
+
+	err = json.Unmarshal(data, &Posts)
+	if err != nil {
+		panic(err)
+	}
+
+	temp, err := template.ParseFiles("./post.html")
+	if err != nil {
+		panic(err)
+	}
+
+	temp.Execute(res, Posts)
+}
+
+func main() {
+	http.HandleFunc("/posts", homeHandler)
+
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
