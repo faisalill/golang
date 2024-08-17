4337238415a05e54821e0845feae74205e710b3e
faisalill <mfaisal.ilyas.d@gmail.com>
2024-08-17 22:34:26 +0530

    wbapp new post dn

--- wbapp/main.go
+++ wbapp/main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"database/sql"
 	"fmt"
+	"html/template"
 	"log"
 
+	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
 )
 
 type Post struct {
-	id     int32
-	userid int32
-	title  string
-	body   string
+	Id     int32
+	Userid int32
+	Title  string
+	Body   string
 }
 
 func checkErr(err error) {
@@ -55,6 +58,34 @@ func deletePost(db *sql.DB, id int32, userid int32) {
 	fmt.Println("Succesfully Deleted Post")
 }
 
+func showPostsHandler(db *sql.DB) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		posts := getPosts(db)
+
+		temp, err := template.ParseFiles("posts.html")
+		checkErr(err)
+
+		var buf bytes.Buffer
+		err = temp.ExecuteTemplate(&buf, "posts.html", posts)
+		checkErr(err)
+
+		c.Set("Content-Type", "text/html; charset=utf-8")
+		return c.SendString(buf.String())
+	}
+}
+
+func formHandler(c *fiber.Ctx) error {
+	temp, err := template.ParseFiles("form.html")
+	checkErr(err)
+
+	var buf bytes.Buffer
+	err = temp.ExecuteTemplate(&buf, "form.html", nil)
+
+	c.Set("Content-Type", "text/html; charset=utf-8")
+
+	return c.SendString(buf.String())
+}
+
 func main() {
 	db, err := sql.Open("postgres", "user=postgres password=password dbname=wbapp sslmode=disable")
 	checkErr(err)
@@ -63,8 +94,28 @@ func main() {
 	checkErr(err)
 
 	fmt.Println("Connected to Database Successfully")
+	fmt.Println("Starting Server")
 
-	posts := getPosts(db)
+	app := fiber.New()
+	app.Use(func(c *fiber.Ctx) error {
+		if c.Path() != "/posts" && c.Path() != "/new" && c.Path() != "/createpost" {
+			return c.Redirect("/posts")
+		}
 
-	fmt.Println(posts)
+		return c.Next()
+	})
+
+	app.Get("/posts", showPostsHandler(db))
+	app.Get("/new", formHandler)
+	app.Post("/createpost", func(c *fiber.Ctx) error {
+		title := c.FormValue("title")
+		body := c.FormValue("body")
+
+		fmt.Printf("Title: %s Body: %s", title, body)
+
+		c.Set("Content-Type", "text/html; charset=utf-8")
+		return c.SendString("<h1>Post Created Successfully</h1>")
+	})
+
+	log.Fatal(app.Listen(":3000"))
 }
